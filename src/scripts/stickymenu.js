7acdd3dbd6e6e17b9282651bbeed56ed49d81848
chelunike <chelunike@gmail.com>
2024-02-19 01:10:22 +0100

    HotFix navbar logo close #25

--- src/scripts/stickymenu.js
+++ src/scripts/stickymenu.js
@@ -13,24 +13,6 @@ document.addEventListener('DOMContentLoaded', function () {
       ud_header.classList.add('absolute', 'bg-transparent', 'z-40');
     }
 
-    if(logo.length) {
-      // === logo change
-      if (ud_header.classList.contains('sticky-header')) {
-        document.querySelector(".header-logo").src = '/assets/logo/logo.svg';
-      } else {
-        document.querySelector('.header-logo').src = '/assets/logo/logo-white.svg';
-      }
-    }
-
-    if (document.documentElement.classList.contains('dark')) {
-      if (logo.length) {
-        // === logo change
-        if (ud_header.classList.contains('sticky-header')) {
-          document.querySelector('.header-logo').src = '/assets/logo/logo-white.svg';
-        } 
-      }
-    }
-
     // show or hide the back-top-top button
     const backToTop = document.querySelector('.back-to-top');
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
