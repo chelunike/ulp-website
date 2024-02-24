394afe10d06406b16e4838c6b4b5f34eaba52d11
chelunike <chelunike@gmail.com>
2024-02-24 08:43:29 +0100

    Added Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress(),]
+  site: 'https://ulp.ugr.es',
+  integrations: [tailwind(), compress(), sitemap()]
 });
